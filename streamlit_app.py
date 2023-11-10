58d71c1084ba9ac6ef3f0cadc5e6fbe542b46e15
Fabian <fabian.witt@t-online.de>
2023-11-10 11:47:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ except URLError as e:
 
 streamlit.header("The fruit list containers:")
 
-def get_fruit_load_list()
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM fruit_load_list")
     return my_cur.fetchall()

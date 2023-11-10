2363050b15c5d2a522ca6fb8567656e52a3b7d02
Fabian <fabian.witt@t-online.de>
2023-11-10 11:40:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,8 @@ try:
 except URLError as e:
   streamlit.error()
 
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")

666d092bfc7eccef87876942f3bad623a8a3e647
Fabian <fabian.witt@t-online.de>
2023-11-10 11:33:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,6 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit list containers:")
-streamlit.text(my_data_row)
+
+streamlit.header("The fruit list containers:")
+streamlit.dataframe(my_data_row)
